c8d9b5de611b7649836f1d8e7c08e14bb092672d
whdgur5717 <whdgur5717@gmail.com>
2026-01-26 15:48:34 +0900

    🔧 chore: add shared eslint and tsconfig packages
    - Add @design-editor/eslint-config with import sort and prettier
    - Add @design-editor/tsconfig with base configuration
    - Add pnpm catalog for unified react versions

---
+++ config/eslint-config/index.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js"
+import tseslint from "typescript-eslint"
+import tsParser from "@typescript-eslint/parser"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import eslintConfigPrettier from "eslint-config-prettier"
+
+export default [
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		ignores: ["node_modules/**", "dist/**", "*.config.{js,ts}"],
+	},
+	{
+		plugins: {
+			"simple-import-sort": simpleImportSort,
+		},
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				projectService: true,
+			},
+		},
+		rules: {
+			"@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error",
+		},
+	},
+	eslintConfigPrettier,
+]
